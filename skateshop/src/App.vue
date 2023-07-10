da180e6b546fa708f52069b48e58a32926f23f92
Jefferson Mattos <jefflmattos@gmail.com>
2023-07-10 20:21:02 -0300

    done!

--- skateshop/src/App.vue
+++ skateshop/src/App.vue
@@ -24,7 +24,8 @@
 * {
   margin: 0;
   padding: 0;
-  position: relative
+  position: relative;
+  font-family: 'Roboto', sans-serif;
 }
 
 
--- skateshop/src/components/FilterProduct.vue
+++ skateshop/src/components/FilterProduct.vue
@@ -1,15 +1,16 @@
 <template>
     <div>
-      <input v-bind:value="value" 
-      v-on:input="$emit('input', $event.target.value)"
-      placeholder="Search products">
+      <input 
+        :value="modelValue" 
+        @input="$emit('update:modelValue', $event.target.value)"
+        placeholder="Search products">
     </div>
   </template>
   
 <script>
 export default {
     name: "FilterProduct",
-    props: ['value']
+    props: ['modelValue']
     }
 
 </script>
@@ -17,7 +18,7 @@ export default {
 <style scope>
     input {
         width: 200px;
-        margin: 5px;
+        margin: 15px;
         position: relative;
         left: 77vw;
     }
